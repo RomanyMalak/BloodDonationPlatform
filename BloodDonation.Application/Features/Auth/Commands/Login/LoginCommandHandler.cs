b0e9d7a333bd328eb58d1c10d7f0be594bc24481
MernaSobhi <mernasobhi3@gmail.com>
2026-06-03 03:18:35 +0300

    logincommand

---
+++ BloodDonation.Application/Features/Auth/Commands/Login/LoginCommandHandler.cs
@@ -0,0 +1,43 @@
+﻿using BloodDonation.Application.Interfaces;
+using MediatR;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BloodDonation.Application.Features.Auth.Commands.Login
+{
+    public class LoginCommandHandler : IRequestHandler<LoginCommand, LoginResponseDto>
+    {
+        private readonly IApplicationDbContext _context;
+        private readonly IJwtTokenGenerator _jwtTokenGenerator;
+        public LoginCommandHandler(IApplicationDbContext context, IJwtTokenGenerator jwtTokenGenerator)
+        {
+            _context = context;
+            _jwtTokenGenerator = jwtTokenGenerator;
+        }
+        public async Task<LoginResponseDto> Handle(LoginCommand request, CancellationToken cancellationToken)
+        {
+            var user = await _context.Users.FirstOrDefaultAsync(
+
+          x => x.Email == request.Email,
+          cancellationToken);
+            if (user == null)
+            {
+                throw new UnauthorizedAccessException("Invalid email or password ");
+            }
+            bool isPasswordValid = BCrypt.Net.BCrypt.Verify(request.Password, user.PasswordHash);
+            if (!isPasswordValid)
+            {
+                throw new UnauthorizedAccessException("Invalid  email or password");
+            }
+
+            var token = _jwtTokenGenerator.GenerateToken(user);
+
+            return await Task.FromResult(new LoginResponseDto
+                (user.Id, user.FullName, user.Email, user.Role.ToString(), token));
+        }
+    }
+}
