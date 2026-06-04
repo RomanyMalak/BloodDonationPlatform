89a5245dd8a945a6c33384d1e7c86a6ad516bbfd
Remonda Nady <remonda.n.s753@gmail.com>
2026-06-04 16:33:11 +0300

    InfrastructureServiceExtensions

---
+++ BloodDonation.Infrastructure/Extensions/InfrastructureServiceExtensions.cs
@@ -0,0 +1,30 @@
+﻿using BloodDonation.Application.Interfaces;
+using BloodDonation.Application.Interfaces.Repositories;
+using BloodDonation.Infrastructure.Persistence;
+using BloodDonation.Infrastructure.Repositories;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace BloodDonation.Infrastructure.Extensions;
+
+public static class InfrastructureServiceExtensions
+{
+    public static IServiceCollection AddInfrastructure(
+        this IServiceCollection services,
+        IConfiguration configuration)
+    {
+        // Database
+        services.AddDbContext<AppDbContext>(options =>
+            options.UseSqlServer(
+                configuration.GetConnectionString("DefaultConnection")));
+
+        services.AddScoped<IApplicationDbContext>(sp =>
+            sp.GetRequiredService<AppDbContext>());
+
+        // Repositories
+        services.AddScoped<IBloodRequestRepository, BloodRequestRepository>();
+
+        return services;
+    }
+}
