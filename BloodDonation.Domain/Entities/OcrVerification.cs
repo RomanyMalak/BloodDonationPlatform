34cd4f0769671dfdeb257cfd165018314712cf4f
Remonda Nady <remonda.n.s753@gmail.com>
2026-06-04 16:36:02 +0300

    add OcrVerification entity

---
+++ BloodDonation.Domain/Entities/OcrVerification.cs
@@ -0,0 +1,15 @@
+﻿namespace BloodDonation.Domain.Entities;
+
+public class OcrVerification
+{
+    public Guid Id { get; set; }
+    public Guid BloodRequestId { get; set; }
+    public bool IsVerified { get; set; }
+    public double ConfidenceScore { get; set; }
+    public string? RawExtractedText { get; set; }
+    public string? FailureReason { get; set; }
+    public DateTime VerifiedAt { get; set; }
+    public BloodRequest BloodRequest { get; set; } = null!;
+}  
+
+
