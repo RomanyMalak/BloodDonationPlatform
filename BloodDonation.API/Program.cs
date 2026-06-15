d617e58428e752d34ddec94627c995f4a6ee9b57
MernaSobhi <mernasobhi3@gmail.com>
2026-06-15 16:03:27 +0300

    add commint

--- BloodDonation.API/Program.cs
+++ BloodDonation.API/Program.cs
@@ -14,7 +14,7 @@ var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddApplication();
 
-builder.Services.AddInfrastructure(builder.Configuration);
+builder.Services.AddInfrastructure(builder.Configuration); //استدعاء الـ Extension Method
 
 builder.Services.AddControllers();
 
@@ -38,7 +38,7 @@ builder.Services.AddCors(options =>
 #endregion
 
 #region Authentication
-
+// JWT Authentication setings
 builder.Services.AddAuthentication(options =>
 {
     options.DefaultAuthenticateScheme =
