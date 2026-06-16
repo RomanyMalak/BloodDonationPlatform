533d8b9151ffd5f222e2263262ad8c29266c447e
MernaSobhi <mernasobhi3@gmail.com>
2026-06-16 21:59:22 +0300

    try to publish

--- BloodDonation.Infrastructure/Persistence/AppDbContextFactory.cs
+++ BloodDonation.Infrastructure/Persistence/AppDbContextFactory.cs
@@ -1,4 +1,5 @@
 
+using BloodDonation.Domain.Entities;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Design;
 
@@ -9,11 +10,10 @@ public class AppDbContextFactory : IDesignTimeDbContextFactory<AppDbContext>
     public AppDbContext CreateDbContext(string[] args)
     {
         var optionsBuilder = new DbContextOptionsBuilder<AppDbContext>();
-        optionsBuilder.UseSqlServer("Server=.;Database=BloodDonationDb;Trusted_Connection=True;TrustServerCertificate=True;");
-      //  optionsBuilder.UseSqlServer("Server=DESKTOP-A3J4UMH\\SQLEXPRESS;Database=BloodDonationDb;Trusted_Connection=True;TrustServerCertificate=True;");
-
+       //optionsBuilder.UseSqlServer("Server=.;Database=BloodDonationDb;Trusted_Connection=True;TrustServerCertificate=True;");
+        //  optionsBuilder.UseSqlServer("Server=DESKTOP-A3J4UMH\\SQLEXPRESS;Database=BloodDonationDb;Trusted_Connection=True;TrustServerCertificate=True;");
+        optionsBuilder.UseSqlServer("Server=db56229.databaseasp.net; Database=db56229; User Id=db56229; Password=Am5_Bo2J; Encrypt=False; MultipleActiveResultSets=True;");
         return new AppDbContext(optionsBuilder.Options);
     }
 }
-
-
+                                                     
