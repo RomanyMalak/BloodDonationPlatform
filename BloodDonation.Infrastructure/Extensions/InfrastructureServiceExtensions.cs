4046ed3cd3dc0dec16e79f5fb9022ba208114af6
RomanyMalak <romany.malak.kamel.sabet@gmail.com>
2026-06-20 16:11:06 +0300

    Add notification agent service interfaces

--- BloodDonation.Infrastructure/Extensions/InfrastructureServiceExtensions.cs
+++ BloodDonation.Infrastructure/Extensions/InfrastructureServiceExtensions.cs
@@ -1,8 +1,10 @@
 ﻿using BloodDonation.Application.Interfaces;
 using BloodDonation.Application.Interfaces.Repositories;
+using BloodDonation.Infrastructure.Hubs;
 using BloodDonation.Infrastructure.Persistence;
 using BloodDonation.Infrastructure.Repositories;
 using BloodDonation.Infrastructure.Services;
+using Microsoft.AspNetCore.SignalR;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
@@ -29,8 +31,14 @@ public static class InfrastructureServiceExtensions
         // Services
         services.AddScoped<IJwtTokenGenerator, JwtTokenGenerator>(); 
         services.AddScoped<IDonorService, DonorService>();
+        services.AddScoped<IDonorMatchingService, DonorMatchingService>();
+        services.AddScoped<IAiMatchingPipelineService, NotificationAgentService>();
         services.AddScoped<IDashboardService, DashboardService>();
         services.AddScoped<INotificationService, NotificationService>();
+        services.AddScoped<INotificationSender, SignalRNotificationSender>();
+        services.AddSingleton<IUserIdProvider, NotificationUserIdProvider>();
+        services.AddSingleton<INotificationAgentQueue, NotificationAgentQueue>();
+        services.AddHostedService<NotificationAgentBackgroundService>();
         services.AddScoped<IOcrService, OcrService>();
         services.AddSingleton<IOcrVerificationQueue, OcrVerificationQueue>();
         services.AddHostedService<OcrBackgroundService>();
